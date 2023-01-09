b322c951ff8368faf3136f47b9382abbfc1e3076
Guy Nahmani <snitzel81@gmail.com>
2023-01-09 11:55:54 +0200

    second commit

---
+++ controllers/goalController.js
@@ -0,0 +1,14 @@
+//import { express } from "express";
+//express.getGoals = 
+//const getGoals = express.Router();
+ const getGoals = (req,res) => {(
+
+    res.status(200).json({ messege: 'Get Goals', })
+
+)};
+const setGoals = (req,res) => {(
+
+    res.status(200).json({ messege: 'Get Goals', })
+
+)};
+export { getGoals  };
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import { goalRoutes } from "./routes/goalRoutes.js"
+
+dotenv.config();
 
 
 const app = express();
 
-const PORT = 4000;
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/goals', goalRoutes);
 
 
-app.listen(PORT,()=>console.log("server is running "));
+app.listen(port, () => console.log("server is running "));
+
+
 
 
