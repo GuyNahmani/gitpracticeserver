3dcb4f206240332f2a691cfb8440a731b903c839
Guy Nahmani <snitzel81@gmail.com>
2023-01-09 14:28:47 +0200

    controller finised

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -2,13 +2,18 @@
 //express.getGoals = 
 //const getGoals = express.Router();
  const getGoals = (req,res) => {(
-
     res.status(200).json({ messege: 'Get Goals', })
+) };
 
-)};
 const setGoals = (req,res) => {(
-
-    res.status(200).json({ messege: 'Get Goals', })
+    res.status(200).json({ messege: 'Set Goals', })
+)};
+const putGoals = (req,res) => {(
+    res.status(200).json({ messege: `put Goals ${req.params.id}`, })
+)};
+const deleteGoals = (req,res) => {(
+    res.status(200).json({ messege: `delete Goals ${req.params.id}`, })
 
 )};
-export { getGoals  };
+export { getGoals,setGoals,putGoals,deleteGoals  };
+
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,28 +1,16 @@
 import express from "express";
 const router = express.Router();
 //import { getGoals } from "../controllers/goalController.js";
-import { getGoals  } from "../controllers/goalController.js"
+import { getGoals,setGoals,putGoals,deleteGoals  } from "../controllers/goalController.js"
  
 
 
 //console.log(getGoals) 
 // show me the info inside my requast 
-//router.get('/', getGoals);
 
+router.route('/').get(getGoals).post(setGoals);
+
+router.route('/:id').delete(deleteGoals).put(putGoals);
 // using post i insert info to my backend - when i use mongos
-router.post('/', (req, res) => {
-    res.status(200).json({
-        messege: `${req.body.messege}`
-    });
 
-
-});
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ messege: `Update Goals ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ messege: `delete Goals ${req.params.id}`, });
-});
 export { router as goalRoutes };
